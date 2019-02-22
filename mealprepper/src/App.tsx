15ba95c9b446be6fd065b21f0394e388f56f420f
Michael R Roark <michaelrroark@gmail.com>
2019-02-22 14:59:20 -0500

    Integrated auth buttons to source code. Setup amplify configurations.

--- mealprepper/src/App.tsx
+++ mealprepper/src/App.tsx
@@ -5,6 +5,18 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './Store';
 import  NavComponent  from './Components/Nav/Nav.component';
+import Amplify from 'aws-amplify';
+import config from './config/cognito.config'
+
+Amplify.configure({
+  Auth : {
+    mandatorySignIn : true,
+    region : config.cognito.REGION,
+    userPoolId : config.cognito.USER_POOL_ID,
+    identityPoolId : config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId : config.cognito.APP_CLIENT_ID
+  }
+});
 
 class App extends Component {
   render() {
