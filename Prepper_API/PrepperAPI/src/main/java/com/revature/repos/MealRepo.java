a9d69a6fde0eff5ef791a172161f712ab13290fc
Michael R Roark <michaelrroark@gmail.com>
2019-02-27 12:47:48 -0500

    Implemented meal controller and associated classes and interfaces. Implemented generate endpoint for meal.

--- Prepper_API/PrepperAPI/src/main/java/com/revature/repos/MealRepo.java
+++ Prepper_API/PrepperAPI/src/main/java/com/revature/repos/MealRepo.java
@@ -2,9 +2,11 @@ package com.revature.repos;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.revature.model.Meal;
 
-public interface MealRepo {
+public interface MealRepo extends JpaRepository<Meal, Integer> {
 	List<Meal> findByName(String name);
 	Meal findById(int id);
 }
